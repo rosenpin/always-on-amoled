4021b30a41cf44501271569214c8c4f3f8ade7bd
Tomer Rosenfeld <tomerosenfeld007@gmail.com>
2016-06-11 02:50:17 +0300

    fixed untracked files

--- app/src/main/java/com/tomer/alwayson/SecretConstants.java
+++
@@ -1,9 +0,0 @@
-package com.tomer.alwayson;
-
-/**
- * Created by tomer on 6/11/16.
- */
-public class SecretConstants {
-    public static String IAPID = "smalldonation";
-    public static String GoogleIAPCode = "bGoa+V7g/yqDXvKRqq+JTFn4uQZbPiQJo4pf9RzJ";
-}
