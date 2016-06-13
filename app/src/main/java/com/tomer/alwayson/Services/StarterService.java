656eb0cf27d297bb92e04d9378c723d59b96aa76
Tomer Rosenfeld <tomerosenfeld007@gmail.com>
2016-06-13 21:51:55 +0300

    Manually merged Maxr1998  140682d PR

--- app/src/main/java/com/tomer/alwayson/Services/StarterService.java
+++ app/src/main/java/com/tomer/alwayson/Services/StarterService.java
@@ -4,7 +4,6 @@ import android.app.Notification;
 import android.app.NotificationManager;
 import android.app.Service;
 import android.content.BroadcastReceiver;
-import android.content.Context;
 import android.content.Intent;
 import android.content.IntentFilter;
 import android.os.IBinder;
@@ -15,14 +14,35 @@ import com.tomer.alwayson.R;
 import com.tomer.alwayson.Receivers.ScreenReceiver;
 
 public class StarterService extends Service {
-    Prefs prefs;
-    BroadcastReceiver mReceiver;
-    IntentFilter filter;
+    private BroadcastReceiver mReceiver;
+    private boolean isRegistered = false;
 
     @Nullable
     @Override
     public IBinder onBind(Intent intent) {
         return null;
+    }
+
+    @Override
+    public void onCreate() {
+        super.onCreate();
+        System.out.println("Starter Service started");
+
+        Prefs prefs = new Prefs(getApplicationContext());
+        prefs.apply();
+
+        if (prefs.enabled) {
+            if (prefs.showNotification) {
+                showNotification();
+            }
+            if (prefs.notificationsAlerts) {
+                startService(new Intent(getApplicationContext(), NotificationListener.class));
+            }
+            registerReceiver();
+        } else {
+            hideNotification();
+            unregisterReceiver();
+        }
     }
 
     @Override
@@ -32,39 +52,6 @@ public class StarterService extends Service {
         unregisterReceiver();
     }
 
-    @Override
-    public void onCreate() {
-        super.onCreate();
-
-        System.out.println("Starter Service started");
-
-        prefs = new Prefs(getApplicationContext());
-        prefs.apply();
-
-        filter = new IntentFilter(Intent.ACTION_SCREEN_ON);
-        filter.addAction(Intent.ACTION_SCREEN_OFF);
-        filter.addAction(Intent.ACTION_USER_PRESENT);
-        mReceiver = new ScreenReceiver();
-
-        if (prefs.showNotification)
-            showNotification();
-        else
-            hideNotification();
-
-        if (prefs.enabled) {
-            unregisterReceiver();
-            startService(new Intent(getApplicationContext(), NotificationListener.class));
-            registerReceiver(mReceiver, filter);
-            if (prefs.notificationsAlerts) {
-                startService(new Intent(getApplicationContext(), NotificationListener.class));
-            }
-        } else {
-            hideNotification();
-            unregisterReceiver();
-        }
-
-    }
-
     private void showNotification() {
         Notification.Builder builder = new Notification.Builder(getApplicationContext());
         builder.setContentTitle(getString(R.string.notification_message));
@@ -72,27 +59,38 @@ public class StarterService extends Service {
         builder.setPriority(Notification.PRIORITY_MIN);
         builder.setSmallIcon(android.R.color.transparent);
         Notification notification = builder.build();
-        NotificationManager notificationManger =
-                (NotificationManager) getSystemService(Context.NOTIFICATION_SERVICE);
-        notificationManger.notify(01, notification);
+        NotificationManager notificationManger = (NotificationManager) getSystemService(NOTIFICATION_SERVICE);
+        notificationManger.notify(1, notification);
     }
 
     private void hideNotification() {
-        String ns = Context.NOTIFICATION_SERVICE;
-        NotificationManager nMgr = (NotificationManager) getApplicationContext().getSystemService(ns);
+        NotificationManager nMgr = (NotificationManager) getApplicationContext().getSystemService(NOTIFICATION_SERVICE);
         nMgr.cancelAll();
     }
 
-    void unregisterReceiver() {
+    private void registerReceiver() {
+        unregisterReceiver();
+        IntentFilter filter = new IntentFilter(Intent.ACTION_SCREEN_ON);
+        filter.addAction(Intent.ACTION_SCREEN_OFF);
+        filter.addAction(Intent.ACTION_USER_PRESENT);
+
+        mReceiver = new ScreenReceiver();
+        registerReceiver(mReceiver, filter);
+        isRegistered = true;
+    }
+
+    private void unregisterReceiver() {
+        if (!isRegistered) {
+            return;
+        }
         try {
             unregisterReceiver(mReceiver);
-        } catch (IllegalArgumentException ignored) {
-        }
-
-        try {
-            mReceiver.abortBroadcast();
+            if (mReceiver.isOrderedBroadcast())
+                mReceiver.abortBroadcast();
         } catch (Exception e) {
             e.printStackTrace();
+        } finally {
+            isRegistered = false;
         }
     }
 }
