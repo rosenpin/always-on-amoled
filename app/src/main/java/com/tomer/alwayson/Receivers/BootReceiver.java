f3e1d47b169805c6d6124388d381579c26117f0a
Tomer Rosenfeld <tomerosenfeld007@gmail.com>
2016-06-13 03:53:29 +0300

    Fixed start on boot

--- app/src/main/java/com/tomer/alwayson/Receivers/BootReceiver.java
+++ app/src/main/java/com/tomer/alwayson/Receivers/BootReceiver.java
@@ -3,6 +3,7 @@ package com.tomer.alwayson.Receivers;
 import android.content.BroadcastReceiver;
 import android.content.Context;
 import android.content.Intent;
+import android.util.Log;
 
 import com.tomer.alwayson.Prefs;
 import com.tomer.alwayson.Services.StarterService;
@@ -14,11 +15,11 @@ import com.tomer.alwayson.Services.StarterService;
 public class BootReceiver extends BroadcastReceiver {
     @Override
     public void onReceive(Context context, Intent intent) {
-        if (intent.getAction().equals(Intent.ACTION_BOOT_COMPLETED)) {
-            Prefs prefs = new Prefs(context);
-            if (prefs.enabled) {
-                context.startService(new Intent(context, StarterService.class));
-            }
+        Prefs prefs = new Prefs(context);
+        prefs.apply();
+        if (prefs.enabled) {
+            context.startService(new Intent(context, StarterService.class));
+            Log.i("Autostart Always On ", "started");
         }
     }
 
